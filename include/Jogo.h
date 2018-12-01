8ba4a1d89474ef62cf5e1bcbf0b2304a00e3954e
JoaoPedroAssis <joapedroassisdossantos@gmail.com>
2018-12-01 15:21:37 -0200

    Ajeitando algumas coisas

--- include/Jogo.h
+++ include/Jogo.h
@@ -1,4 +1,6 @@
+// copyright 2018 Switch Dreams
 #pragma once
+#include <stdlib.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
@@ -10,12 +12,11 @@
 #include "GeraRecursos.h"
 #include "Unidade.h"
 #include <iostream>
-#include <stdlib.h>
 
 
 
 class Jogo {
-public:
+ public:
     Jogo();
     void init(const char* titulo, int x_pos, int y_pos, int width, int height);
     bool loadMidia();
@@ -36,33 +37,34 @@ public:
     SDL_Texture* texturas[TEXTURAS::TEX_TOTAL];
     bool menu_inicial = false;
     SDL_Texture* aloo;
-    //#ifdef TESTE
-    //Player* jogador = NULL;
-    //Player* cpu = NULL;
-    //#endif
-    //Player* jogador = NULL;
+    // #ifdef TESTE
+    // Player* jogador = NULL;
+    // Player* cpu = NULL;
+    // #endif
+    // Player* jogador = NULL;
     bool comprando = false;
     TIPO tipoCompra = NADA;
     int nivelCompra = 0;
-    //SDL_Texture *teste;
+    // SDL_Texture *teste;
 
     /* Movimentacao */
     void movimentacao();
-protected:
+
+ protected:
     Objeto* recursoDinheiroJogador = NULL;
-    Objeto* recursoCeluloseJogador= NULL;
-    Objeto* recursoPedregulhoJogador= NULL;
-    Objeto* recursoMetalJogador= NULL;
-    Objeto* recursoDinheiroCpu= NULL;
-    Objeto* recursoCeluloseCpu= NULL;
-    Objeto* recursoPedregulhoCpu= NULL;
-    Objeto* recursoMetalCpu= NULL;
+    Objeto* recursoCeluloseJogador = NULL;
+    Objeto* recursoPedregulhoJogador = NULL;
+    Objeto* recursoMetalJogador = NULL;
+    Objeto* recursoDinheiroCpu = NULL;
+    Objeto* recursoCeluloseCpu = NULL;
+    Objeto* recursoPedregulhoCpu = NULL;
+    Objeto* recursoMetalCpu = NULL;
     Objeto* tempo_Obj = NULL;
 
-    SDL_Color cRecurso{0, 0, 0}; //decidir cor
-    SDL_Texture* tRecurso; //talvez possa remover 
+    SDL_Color cRecurso{0, 0, 0};  // decidir cor
+    SDL_Texture* tRecurso;  // talvez possa remover
     TTF_Font* font = NULL;
-    Botao_Compra* compra= NULL;
+    Botao_Compra* compra = NULL;
     Botao_Iniciar* bIniciar = NULL;
     Objeto* menuInicial = NULL;
     Objeto* mapa = NULL;
@@ -71,7 +73,6 @@ protected:
     SDL_Window* janela = NULL;
 
     SDL_Renderer* render = NULL;
-
 };
 
 int combate_unidade(Unidade** unidade1, Unidade** unidade2);
--- source/teste/testeJogo.cpp
+++ source/teste/testeJogo.cpp
@@ -1,5 +1,5 @@
 //
-// Created by pedro on 29/11/18.
+// Copyright [2018] <Waliff Cordeiro>"
 //
 
 #include "../include/catch.hpp"
@@ -8,8 +8,6 @@
 TEST_CASE("Anda") {
     Unidade* atual = new Unidade(0, 0, UNIDADE::TESOURA, 300, 1, 2);
     Unidade* destino = NULL;
-    printf("%d\n", destino);
-    printf("%d", atual);
     anda(&atual, &destino);
     CHECK(atual == NULL);
     REQUIRE(destino->getVelocidade() == 1);
@@ -22,27 +20,91 @@ TEST_CASE("Anda") {
 
 
 TEST_CASE("Combate Unidade") {
-    //X, Y, tipo, vida, velocidade, dano
-    Unidade* unidade1 = new Unidade(2, 20, UNIDADE::PAPEL, 230, 1, 230);
-    Unidade* unidade2 = new Unidade(1, 20, UNIDADE::PAPEL, 231, -1, 230);
-    SECTION("Combate Unidade1 morre") {
-        combate_unidade(&unidade1, &unidade2);
-        CHECK(unidade1 == NULL);
-        REQUIRE(unidade2->getVida() == 1);
-    }
+    // X, Y, tipo, vida, velocidade, dano
+    Unidade* unidade1 = new Unidade(2, 20, UNIDADE::PAPEL, 200, 1, 100);
+    Unidade* unidade2 = new Unidade(1, 20, UNIDADE::PEDRA, 200, -1, 100);
+
     SECTION("Combate Unidade2 morre") {
+        combate_unidade(&unidade1, &unidade2);
+        REQUIRE(combate_unidade(&unidade1, &unidade2) == 2);
+        REQUIRE(unidade1->getVida() >= 0);
+        REQUIRE(unidade2->getVida() <= 0);
+}
+SECTION("Combate Unidade2 morre") {
         unidade2->setTipo(UNIDADE::PEDRA);
         combate_unidade(&unidade1, &unidade2);
-        REQUIRE(unidade1->getVida() == 0);
-        REQUIRE(unidade2 == NULL);
+        REQUIRE(combate_unidade(&unidade1, &unidade2) == 2);
+        REQUIRE(unidade1->getVida() == 100);
+        REQUIRE(unidade2->getVida() == 0);
     }
     SECTION("Ambos morrem") {
         unidade2->setTipo(UNIDADE::PAPEL);
-        unidade2->setDano(230);
-        unidade2->setVida(230);
-        combate_unidade(&unidade1, &unidade2);
-        REQUIRE(unidade1 == NULL);
-        REQUIRE(unidade2 == NULL);
+        REQUIRE(combate_unidade(&unidade1, &unidade2) == 3);
+        REQUIRE(unidade1->getVida() <= 0);
+        REQUIRE(unidade2->getVida() <= 0);
+    }
+    SECTION("Dobro de dano") {
+        unidade2->setTipo(UNIDADE::TESOURA);
+        unidade2->setDano(200);
+        unidade2->setVida(1000);
+        REQUIRE(combate_unidade(&unidade1, &unidade2) == 1);
+        REQUIRE(unidade1->getVida() == -200);
+        REQUIRE(unidade2->getVida() == 900);
+    }
+}
+TEST_CASE("Ataca Geradora") {
+    Unidade* unidade = new Unidade(2, 20, UNIDADE::PAPEL, 200, 1, 20);
+    GeraRecursos* geradora = new GeraRecursos(3, 20, UNIDADE::PAPEL);
+    SECTION("Geradora não morre") {
+        geradora->set_vida(21);
+        printf("\ndano unidade: %d\n", unidade->getDano());
+        printf("\nvida geradora: %d\n", geradora->get_vida());
+        REQUIRE(ataca_geraRecurso(&unidade, &geradora) == 0);
+        printf("\ndano unidade: %d\n", unidade->getDano());
+        printf("\nvida geradora: %d\n", geradora->get_vida());
+        REQUIRE(geradora->get_vida() == 1);
+    }
+    SECTION("Geradora morre") {
+        geradora->set_vida(19);
+        REQUIRE(ataca_geraRecurso(&unidade, &geradora) == 1);
+        REQUIRE(geradora->get_vida() == -1);
+    }
+    SECTION("Tropa counter") {
+        unidade->setTipo(UNIDADE::TESOURA);
+        geradora->set_vida(40);
+        REQUIRE(ataca_geraRecurso(&unidade, &geradora) == 1);
+        REQUIRE(geradora->get_vida() == 0);
     }
 }
 
+TEST_CASE("Ataca Fabrica") {
+    Unidade* unidade = new Unidade(2, 20, UNIDADE::PAPEL, 200, 1, 5);
+    Fabrica* fbrc = new Fabrica(3, 20, UNIDADE::PEDRA);
+    SECTION("Fabrica não morre") {
+    fbrc->set_vida(11);
+    REQUIRE(ataca_fabrica(&unidade, &fbrc) == 0);
+    REQUIRE(fbrc->get_vida() == 1);
+    }
+    SECTION("Fabrica morre - counter") {
+    fbrc->set_vida(1);
+    REQUIRE(ataca_fabrica(&unidade, &fbrc) == 1);
+    REQUIRE(fbrc->get_vida() == -9);
+    }
+    unidade->setTipo(UNIDADE::PEDRA);
+    SECTION("Batalhas de mesmo tipo - Fabrica morre") {
+        fbrc->set_vida(4);
+        REQUIRE(ataca_fabrica(&unidade, &fbrc) == 1);
+        REQUIRE(fbrc->get_vida() == -1);
+        fbrc->set_vida(5);
+        REQUIRE(ataca_fabrica(&unidade, &fbrc) == 1);
+        REQUIRE(fbrc->get_vida() == 0);
+    }
+    SECTION("Batalhas de mesmo tipo - Fabrica não morre") {
+        fbrc->set_vida(10);
+        REQUIRE(ataca_fabrica(&unidade, &fbrc) == 0);
+        REQUIRE(fbrc->get_vida() == 5);
+        fbrc->set_vida(100);
+        REQUIRE(ataca_fabrica(&unidade, &fbrc) == 0);
+        REQUIRE(fbrc->get_vida() == 95);
+    }
+}
